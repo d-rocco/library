c88ab9db18b22a4319ea410bdb01442320a4aeae
d-rocco <dhrocco27@gmail.com>
2023-02-02 22:52:44 -0500

    Implemented basic header with 'add book' button
    I plan on making the button interactive for the user next. For app.js, all I've done is put a basic starting point in place.

---
+++ app.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(author, title, numPages, hasBeenRead) {
+  this.author = author;
+  this.title = title;
+  this.numPages = numPages;
+  this.hasBeenRead = hasBeenRead;
+}
+
+function addBookToLibrary(Book) {
+  // do something
+}
