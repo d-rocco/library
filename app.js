02046a4a328b559ef2de02254242ccf5f4e716e9
d-rocco <dhrocco27@gmail.com>
2023-02-09 17:25:51 -0500

    v1.0
    Both buttons for each book are functional, and the books are not stylized. I will probably come back to this project to style it a bit better.

--- app.js
+++ app.js
@@ -1,44 +1,98 @@
 const myLibrary = [];
 
-function Book(title, author, numPages, hasBeenRead) {
+function Book(title, author, numPages, hasBeenRead, index) {
   this.title = title;
   this.author = author;
   this.numPages = numPages;
   this.hasBeenRead = hasBeenRead;
+  const finishedBtn = null;
+  const removeBtn = null;
+  this.index = index;
 }
 
+Book.prototype.setButtons = function setButtons(book, bookCard) {
+  const finishedButton = document.createElement('button');
+  finishedButton.classList.add("finished-btn");
+  if (book.hasBeenRead) {
+    finishedButton.textContent = "Finished";
+  } else {
+    finishedButton.textContent = "Unfinished";
+  }
+  
+  const removeButton = document.createElement('button');
+  removeButton.classList.add("remove-btn");
+  removeButton.textContent = "Remove";
+
+  bookCard.appendChild(finishedButton);
+  bookCard.appendChild(removeButton);
+  book.finishedBtn = finishedButton;
+  book.removeBtn = removeButton;
+} 
+
+const library = document.querySelector(".library");
+
 function displayLibrary() {
-  const library = document.querySelector(".library");
   while (library.firstChild) {
     library.removeChild(library.firstChild);
   }
   for (let i = 0; i < myLibrary.length; i += 1) {
-    const bookCard = document.createElement("div");
-    bookCard.classList.add("book-card");
+    if (myLibrary[i] !== '') {
+      const bookCard = document.createElement("div");
+      bookCard.classList.add("book-card");
+      const book = myLibrary[i];
 
-    const bookTitle = document.createElement("div");
-    bookTitle.classList.add("book-title");
-    bookTitle.textContent = myLibrary[i].title;
+      const bookTitle = document.createElement("div");
+      bookTitle.classList.add("book-title");
+      bookTitle.textContent = book.title;
 
-    const bookAuthor = document.createElement("div");
-    bookAuthor.classList.add("book-author");
-    bookAuthor.textContent = myLibrary[i].author;
+      const bookAuthor = document.createElement("div");
+      bookAuthor.classList.add("book-author");
+      bookAuthor.textContent = book.author;
 
-    const pages = document.createElement("div");
-    pages.classList.add("number-of-pages");
-    pages.textContent = myLibrary[i].numPages;
+      const pages = document.createElement("div");
+      pages.classList.add("number-of-pages");
+      pages.textContent = `${book.numPages} Pages`;
 
-    bookCard.appendChild(bookTitle);
-    bookCard.appendChild(bookAuthor);
-    bookCard.appendChild(pages);
-    library.appendChild(bookCard);
+
+      bookCard.appendChild(bookTitle);
+      bookCard.appendChild(bookAuthor);
+      bookCard.appendChild(pages);
+      book.setButtons(book, bookCard);
+      library.appendChild(bookCard);
+    }
   }
+
+  myLibrary.forEach((book) => {
+    if (book !== '') {
+      book.finishedBtn.addEventListener('click', e => {
+      e.preventDefault();
+      if (book.hasBeenRead) {
+        book.hasBeenRead = false;
+        book.finishedBtn.textContent = "Unfinished";
+      } else {
+        book.hasBeenRead = true;
+        book.finishedBtn.textContent = "Finished";
+      }
+    })
+    }
+  })
+
+  myLibrary.forEach((book) => {
+    if (book !== '') {
+      book.removeBtn.addEventListener('click', e => {
+        e.preventDefault();
+        const card = book.removeBtn.parentNode
+        card.parentNode.removeChild(card);
+        myLibrary.splice(book.index, 1, '');
+        console.log(myLibrary);
+      })
+    }
+  })
 }
 
 function addBookToLibrary(title, author, numPages, hasBeenRead) {
-  const book = new Book(title, author, numPages, hasBeenRead);
+  const book = new Book(title, author, numPages, hasBeenRead, myLibrary.length);
   myLibrary.push(book);
-  console.log(myLibrary);
   displayLibrary();
 }
 
@@ -81,3 +135,4 @@ submitBtn.addEventListener("click", e => {
     addBookBtn.style.display = 'inline-block';
   }
 });
+
